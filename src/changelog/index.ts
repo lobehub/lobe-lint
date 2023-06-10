2c3c741b719dc6cb9d5d2930aab2a33919299761
canisminor1990 <i@canisminor.cc>
2023-06-10 23:19:48 +0800

    🔧 chore(npmrc, package.json, src): Update hoist patterns and packages
    Add public-hoist-patterns to .npmrc and install various packages in package.json. Also update displayTypes in changelog/index.ts and add customTypeMap to support emojis for build, chore, and ci types. Finally, modify commitlint/index.ts to disable header length and footer blank checks.

--- src/changelog/index.ts
+++ src/changelog/index.ts
@@ -1,12 +1,20 @@
 module.exports = {
-  displayTypes: ['feat', 'fix', 'style', 'pref', 'build', 'refactor'],
-  showAuthor: true,
-  showAuthorAvatar: true,
   newlineTimestamp: true,
   reduceHeadingLevel: true,
   addBackToTop: true,
   showSummary: true,
   scopeDisplayName: {
     '*': 'misc',
+  },
+  customTypeMap: {
+    build: {
+      emojj: '📦',
+    },
+    chore: {
+      emojj: '🔧',
+    },
+    ci: {
+      emojj: '👷',
+    },
   },
 };
--- src/commitlint/index.ts
+++ src/commitlint/index.ts
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ['gitmoji'],
+  rules: {
+    'footer-leading-blank': [0, 'never'],
+    'header-max-length': [0, 'never'],
+  },
 };
