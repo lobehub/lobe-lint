158674cda4404277b9d644b51f989d72ed226ac2
CanisMinor <i@canisminor.cc>
2023-06-10 16:24:35 +0800

    ✨ feat: update changelog config

--- src/changelog/index.ts
+++ src/changelog/index.ts
@@ -1,3 +1,12 @@
 module.exports = {
-  displayTypes: ['feat', 'fix', 'style', 'pref', 'build'],
+  displayTypes: ['feat', 'fix', 'style', 'pref', 'build', 'refactor'],
+  showAuthor: true,
+  showAuthorAvatar: true,
+  newlineTimestamp: true,
+  reduceHeadingLevel: true,
+  addBackToTop: true,
+  showSummary: true,
+  scopeDisplayName: {
+    '*': 'misc',
+  },
 };
