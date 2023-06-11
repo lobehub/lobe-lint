738941a05798fd53b40a2d1ee2f5ba34895d5518
canisminor1990 <i@canisminor.cc>
2023-06-11 15:01:56 +0800

    ✨ feat(semantic-release): update configs

--- src/semantic-release/index.ts
+++ src/semantic-release/index.ts
@@ -1,3 +1,37 @@
-export default {
-  extends: ['semantic-release-config-gitmoji'],
-};
+const { createConfig } = require('semantic-release-config-gitmoji/lib/createConfig');
+
+export default createConfig({
+  changelogTitle: `<a name="readme-top"></a>
+
+# Changelog`,
+  releaseRules: [
+    {
+      subject: 'feat*',
+      release: 'minor',
+    },
+    {
+      subject: 'fix*',
+      release: 'patch',
+    },
+    {
+      subject: 'perf*',
+      release: 'patch',
+    },
+    {
+      subject: 'style*',
+      release: 'patch',
+    },
+    {
+      subject: 'refactor*',
+      release: 'patch',
+    },
+    {
+      subject: 'build*',
+      release: 'patch',
+    },
+    {
+      subject: 'BREAKING CHANGE*',
+      release: 'major',
+    },
+  ],
+});
