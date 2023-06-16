847d6a2623e79644182275c2ba9b1c30f1977df9
canisminor1990 <i@canisminor.cc>
2023-06-16 23:14:06 +0800

    💄 style(eslint): Update eslint rules for unicorn/prevent-abbreviations
    This commit updates the 'unicorn/prevent-abbreviations' rule in the eslint configuration file. The rule now has an error level and includes a replacement object with the 'props' value set to false.

--- src/eslint/index.ts
+++ src/eslint/index.ts
@@ -20,9 +20,17 @@ export default {
     'typescript-sort-keys/interface': 'error',
     'typescript-sort-keys/string-enum': 'error',
     'unicorn/filename-case': 0,
-    'unicorn/prefer-logical-operator-over-ternary': 0,
+    'unicorn/no-array-reduce': 0,
+    'unicorn/prefer-logical-operator-over-ternary': 'warn',
     'unicorn/prefer-module': 'warn',
-    'unicorn/prevent-abbreviations': 'warn',
+    'unicorn/prevent-abbreviations': [
+      'error',
+      {
+        replacements: {
+          props: false,
+        },
+      },
+    ],
     'unused-imports/no-unused-imports': 'error',
     'unused-imports/no-unused-vars': [
       'warn',
